04626c1c271fcbd38e141f584b8aef693a465942
Larry Jacobs <lryjacobs@gmail.com>
2022-03-18 15:50:52 +0000

    resolve comments

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,11 +6,9 @@ import moment from 'moment';
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
-  const sortedMessages = messages.sort((a,b)=>b.createdAt > a.createdAt ? -1 : b.createdAt < a.createdAt ? 1 : 0);
-
   return (
     <Box>
-      {sortedMessages.map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
